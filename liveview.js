cd46436408c62ac0e8b790289f0f46e0f2063b00
noelpaul <rosytomy7@gmail.com>
2021-04-17 17:15:33 +0530

    updated

--- liveview.js
+++ liveview.js
@@ -1,12 +1,9 @@
 const systeminput = document.getElementById("systeminput");
 const systemresultbox = document.getElementById("systemresultbox");
 const systemiframe = document.getElementById("systemiframe");
-const systemheader = document.getElementById(systemheader);
-
-const systemdefaulttxt = "<!DOCTYPE html>                                             <html>                                                    <head>                                                   <title>   </title>                                       </head>                                                   <body>                                                        </body>                                                    </html>";
-
-  systeminput.innerText = systemdefaulttxt;
-  
+const systemdefaulttxt = "<!DOCTYPE html>                                             <html>                                                    <head>                                                   <title>   </title>                                       </head>                                                   <body>   </body>                                         </html>";
+systeminput.innerText = systemdefaulttxt;
+ console.log(systemdefaulttxt);
 function run() {
  systemiframe.srcdoc = systeminput.value;
 }
