92d36434949117b810506792bea0ac02c570047d
noelpaul <rosytomy7@gmail.com>
2021-04-17 19:30:39 +0530

    updated

--- liveview.js
+++ liveview.js
@@ -1,21 +1,17 @@
 const systeminput = document.getElementById("systeminput");
 const systemresultbox = document.getElementById("systemresultbox");
 const systemiframe = document.getElementById("systemiframe");
-let systemdefaulttxt ="<!DOCTYPEhtml> <html>  <head>   <title>   </title>  </head>  <body>  </body> </html>";
-const breakString = (str, limit) => {
-  let brokenString = '';
-  for (let i = 0, count = 0; i < str.length; i++) {
-    if (count >= limit && str[i] === ' ') {
-      count = 0;
-      brokenString += '\n';
-    } else {
-      count++;
-      brokenString += str[i];
-    }
-  }
-  return brokenString;
-}
-systeminput.innerHTML = breakString(systemdefaulttxt, 4);
+let systemdefaulttxt = `<!DOCTYPE html>
+<html>
+ <head>
+  <title>     </title>
+ </head>
+ <body>  
+ 
+ </body>
+</html>`;
+
+systeminput.innerHTML = systemdefaulttxt;
 function run() {
  systemiframe.srcdoc = systeminput.value;
 }
