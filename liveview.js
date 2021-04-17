4f9e31ae027a5ae0ecc81e8c44a03cf58f0bb9f6
noelpaul <rosytomy7@gmail.com>
2021-04-17 17:15:27 +0530

    updated

--- liveview.js
+++ liveview.js
@@ -1,9 +1,12 @@
 const systeminput = document.getElementById("systeminput");
 const systemresultbox = document.getElementById("systemresultbox");
 const systemiframe = document.getElementById("systemiframe");
-const systemdefaulttxt = "<!DOCTYPE html>                                             <html>                                                    <head>                                                   <title>   </title>                                       </head>                                                   <body>   </body>                                         </html>";
-systeminput.innerText = systemdefaulttxt;
- console.log(systemdefaulttxt);
+const systemheader = document.getElementById(systemheader);
+
+const systemdefaulttxt = "<!DOCTYPE html>                                             <html>                                                    <head>                                                   <title>   </title>                                       </head>                                                   <body>                                                        </body>                                                    </html>";
+
+  systeminput.innerText = systemdefaulttxt;
+  
 function run() {
  systemiframe.srcdoc = systeminput.value;
 }
