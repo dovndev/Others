6c44fe6e936b5b57c8c96ae11f4ac5764479a60a
noelpaul <rosytomy7@gmail.com>
2021-04-17 18:46:38 +0530

    updated

--- liveview.js
+++ liveview.js
@@ -1,9 +1,21 @@
 const systeminput = document.getElementById("systeminput");
 const systemresultbox = document.getElementById("systemresultbox");
 const systemiframe = document.getElementById("systemiframe");
-const systemdefaulttxt = "<!DOCTYPE html>                                             <html>                                                    <head>                                                   <title>   </title>                                       </head>                                                   <body>   </body>                                         </html>";
-systeminput.innerText = systemdefaulttxt;
- console.log(systemdefaulttxt);
+let systemdefaulttxt ="<!DOCTYPEhtml> <html>  <head>   <title>   </title>  </head>  <body>  </body> </html>";
+const breakString = (str, limit) => {
+  let brokenString = '';
+  for (let i = 0, count = 0; i < str.length; i++) {
+    if (count >= limit && str[i] === ' ') {
+      count = 0;
+      brokenString += '\n';
+    } else {
+      count++;
+      brokenString += str[i];
+    }
+  }
+  return brokenString;
+}
+systeminput.innerHTML = breakString(systemdefaulttxt, 4);
 function run() {
  systemiframe.srcdoc = systeminput.value;
 }
