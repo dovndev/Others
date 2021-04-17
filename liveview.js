39352cde09211cabbd3526808e61aac3251c3188
noelpaul <rosytomy7@gmail.com>
2021-04-17 15:45:47 +0530

    updated

--- liveview.js
+++ liveview.js
@@ -1,7 +1,9 @@
 const systeminput = document.getElementById("systeminput");
 const systemresultbox = document.getElementById("systemresultbox");
 const systemiframe = document.getElementById("systemiframe");
- console.log(systemiframe.srcdoc);
+const systemdefaulttxt = "<!DOCTYPE html>                                             <html>                                                    <head>                                                   <title>   </title>                                       </head>                                                   <body>   </body>                                         </html>";
+systeminput.innerText = systemdefaulttxt;
+ console.log(systemdefaulttxt);
 function run() {
  systemiframe.srcdoc = systeminput.value;
 }
