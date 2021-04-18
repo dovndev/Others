da91c08709d36bf13cfdca3b3093858ed7c6db45
noelpaul <rosytomy7@gmail.com>
2021-04-18 18:59:34 +0530

    updated

--- liveview.js
+++ liveview.js
@@ -1,17 +1,31 @@
 const systeminput = document.getElementById("systeminput");
 const systemresultbox = document.getElementById("systemresultbox");
 const systemiframe = document.getElementById("systemiframe");
+const systemtooltip = document.getElementById("myTooltip");
 let systemdefaulttxt = `<!DOCTYPE html>
 <html>
  <head>
   <title>     </title>
  </head>
  <body>  
- 
+         
  </body>
 </html>`;
 
 systeminput.innerHTML = systemdefaulttxt;
 function run() {
  systemiframe.srcdoc = systeminput.value;
+}
+function copyto() {
+  var copyText = document.getElementById("systeminput");
+  copyText.select();
+  copyText.setSelectionRange(0, 99999);
+  document.execCommand("copy");
+  systemtooltip.innerHTML = "Copied your code";
+}
+function outFunc() {
+  systemtooltip.innerHTML = "Copy to clipboard";
+}
+function changetheme() {
+  
 }
