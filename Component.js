d3301b17cb808e7dd56f33d78e6b635b7f0cdf43
noelpaul <rosytomy101@gmail.com>
2021-05-18 13:54:17 +0530

    updated

---
+++ Component.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const e = React.createElement;
+
+
+
+
+function App(arg) {
+  // body...
+  return (<div> content footer</div>)
+}
+
+
+
+
+
+
+
+
+
+
+
+const domContainer = document.querySelector('#app');
+ReactDOM.render(e(App), domContainer);
