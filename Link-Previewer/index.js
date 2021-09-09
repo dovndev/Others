46a16065b37bdf184041381f0add571eafdfd217
Home <home@pop-os.localdomain>
2021-09-09 14:48:51 +0530

    pushed

---
+++ Link-Previewer/index.js
@@ -0,0 +1,47 @@
+const input = document.querySelector(".input");
+const content = document.getElementById("content");
+
+function GetPreview() {
+  const value = input.value;
+  const url = new URL(
+    `http://api.linkpreview.net/?key=44ab599c49f52aa82bb84040cf5d3748&q=${value}`
+    
+  );
+
+  fetch(url.href)
+    .then((res) => res.json())
+    .then((json) => {
+      console.log(json);
+      const html = ` 
+       <div class="img-cont">
+        <img src="${json.image}">
+       </div>
+       <div class="details">
+        <h2>${json.title}</h2>
+        </br>
+        <h4>${json.description}</h4>
+        </br>
+        Link: <a href="${json.url}">${json.url}</a>
+        </div>`;
+
+      content.innerHTML = html;
+    });
+//     fetch(`https://proclink.p.rapidapi.com/oembed?url=${value}`, {
+// 	"method": "GET",
+// 	"headers": {
+// 		"x-rapidapi-host": "proclink.p.rapidapi.com",
+// 		"x-rapidapi-key": "undefined"
+// 	}
+// })
+// .then(response => {
+// 	console.log(response);
+// })
+// .catch(err => {
+// 	console.error(err);
+// });
+}
+
+document.getElementById('form').addEventListener("submit", function (event) {
+    event.preventDefault();
+});
+GetPreview();
