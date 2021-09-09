17b7368a1fcf8f65679233e2916813dc815a588a
Home <home@pop-os.localdomain>
2021-09-09 18:39:07 +0530

    updated

--- Link-Previewer/index.js
+++ Link-Previewer/index.js
@@ -1,10 +1,10 @@
 const input = document.querySelector(".input");
 const content = document.getElementById("content");
 const load = document.querySelector(".load-cont");
+content.style.border = 'none';
 
 function GetPreview() {
-  content.innerHTML = `<img src="https://icons8.com/preloaders/preloaders/22/Fading%20circles.gif" loading="lazy"/>`;
-  load.innerHTML = `<img id="load" src="https://loading.io/mod/spinner/rosary/index.svg" loading="lazy"/>`;
+  content.innerHTML = `<img src="https://loading.io/mod/spinner/rosary/index.svg" loading="lazy"/>`;
   const value = input.value;
   const url = new URL(
     `https://api.linkpreview.net/?key=44ab599c49f52aa82bb84040cf5d3748&q=${value}`
@@ -25,7 +25,8 @@ function GetPreview() {
         </br>
         Link: <a href="${json.url}">${json.url}</a>
         </div>`;
-      load.innerHTML = "";
+
+      content.style.border = '2px solid cornflowerblue';
       content.innerHTML = html;
     });
 }
@@ -33,7 +34,7 @@ function GetPreview() {
 document.getElementById("form").addEventListener("submit", function (event) {
   event.preventDefault();
 });
-GetPreview();
+
 
 //     fetch(`https://proclink.p.rapidapi.com/oembed?url=${value}`, {
 // 	"method": "GET",
