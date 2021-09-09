fa7f40e82b64af48034d9a0607a54995e1129e9e
Home <home@pop-os.localdomain>
2021-09-09 18:17:18 +0530

    pushed

--- Link-Previewer/index.js
+++ Link-Previewer/index.js
@@ -1,11 +1,20 @@
 const input = document.querySelector(".input");
 const content = document.getElementById("content");
+const load = document.querySelector(".load-cont");
 
 function GetPreview() {
+  content.innerHTML = `<img
+  src="https://icons8.com/preloaders/preloaders/22/Fading%20circles.gif"
+  loading="lazy"
+/>`;
+  load.innerHTML=`<img
+  id="load"
+  src="https://loading.io/mod/spinner/rosary/index.svg"
+  loading="lazy"
+/>`;
   const value = input.value;
   const url = new URL(
     `http://api.linkpreview.net/?key=44ab599c49f52aa82bb84040cf5d3748&q=${value}`
-    
   );
 
   fetch(url.href)
@@ -23,9 +32,17 @@ function GetPreview() {
         </br>
         Link: <a href="${json.url}">${json.url}</a>
         </div>`;
-
+        load.innerHTML="";
       content.innerHTML = html;
     });
+}
+
+document.getElementById("form").addEventListener("submit", function (event) {
+  event.preventDefault();
+});
+GetPreview();
+
+
 //     fetch(`https://proclink.p.rapidapi.com/oembed?url=${value}`, {
 // 	"method": "GET",
 // 	"headers": {
@@ -39,9 +56,3 @@ function GetPreview() {
 // .catch(err => {
 // 	console.error(err);
 // });
-}
-
-document.getElementById('form').addEventListener("submit", function (event) {
-    event.preventDefault();
-});
-GetPreview();
