c3f1a428c2a5c351236609d407f3e51c67ea7f88
noelpaul <rosytomy101@gmail.com>
2021-09-10 20:36:11 +0530

    updated

--- Link-Previewer/index.js
+++ Link-Previewer/index.js
@@ -3,31 +3,34 @@ const content = document.getElementById("content");
 const load = document.querySelector(".load-cont");
 content.style.border = 'none';
 
-function GetPreview() {
-  content.innerHTML = `<img src="https://icons8.com/preloaders/preloaders/22/Fading%20circles.gif" loading="lazy"/>`;
-  const value = input.value;
-  const url = new URL(
-    `https://api.linkpreview.net/?key=44ab599c49f52aa82bb84040cf5d3748&q=${value}`
-  );
+function addimg(elm) {
+  let src = 'https://imgr.search.brave.com/-YVe2niUXNLkAKFAyc-FcqZsIb04M3XjxtWcfmcmuK4/fit/1200/1080/ce/1/aHR0cHM6Ly93d3cu/ZHJvZGQuY29tL2lt/YWdlczE0L3doaXRl/Ny5qcGc';
+  if (elm.src !== src) elm.src = src;
+}
 
-  fetch(url.href)
-    .then((res) => res.json())
-    .then((json) => {
-      console.log(json);
+function GetPreview() {
+  const NewElment = document.createElement('div');
+  NewElment.classList.add('linkbox');
+  NewElment.innerHTML = `<img src="https://icons8.com/preloaders/preloaders/22/Fading%20circles.gif" loading="lazy"/>`;
+  content.appendChild(NewElment);
+
+  fetch(`https://api.linkpreview.net/?key=44ab599c49f52aa82bb84040cf5d3748&q=${input.value}`)
+    .then(res => res.json())
+    .then(json => {
+      const url = new URL(json.url);
       const html = ` 
        <div class="img-cont">
-        <img src="${json.image}">
+        <img src="${json.image}" onerror="addimg(this)">
        </div>
        <div class="details">
-        <h2>${json.title}</h2>
-        </br>
-        <h4>${json.description}</h4>
-        </br>
-        Link: <a href="${json.url}">${json.url}</a>
+        <h3>${json.title}</h3>
+        <h5>${json.description}</h5>
+        <a href="${json.url}">${url.hostname}</a>
         </div>`;
-
-      content.style.border = '2px solid cornflowerblue';
-      content.innerHTML = html;
+      
+      NewElment.style.border = '2px solid cornflowerblue';
+      NewElment.innerHTML = html;
+      console.log(json.image)
     });
 }
 
