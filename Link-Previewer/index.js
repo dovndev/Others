f8912f788850c921e7216fef04fc284ea625e451
noelpaul <rosytomy101@gmail.com>
2021-09-11 07:27:27 +0530

    updated

--- Link-Previewer/index.js
+++ Link-Previewer/index.js
@@ -4,11 +4,15 @@ const load = document.querySelector(".load-cont");
 content.style.border = 'none';
 
 function addimg(elm) {
-  let src = 'https://imgr.search.brave.com/-YVe2niUXNLkAKFAyc-FcqZsIb04M3XjxtWcfmcmuK4/fit/1200/1080/ce/1/aHR0cHM6Ly93d3cu/ZHJvZGQuY29tL2lt/YWdlczE0L3doaXRl/Ny5qcGc';
-  if (elm.src !== src) elm.src = src;
+  const details = elm.parentNode.parentNode.lastChild;
+  details.classList.add('full');
+  elm.parentNode.remove();
 }
 
+const regexp = /[^\.]\.[^\.]/;
+
 function GetPreview() {
+  if (!(regexp.test(input.value))) return alert('Not A Link');
   const NewElment = document.createElement('div');
   NewElment.classList.add('linkbox');
   NewElment.innerHTML = `<img src="https://icons8.com/preloaders/preloaders/22/Fading%20circles.gif" loading="lazy"/>`;
@@ -17,20 +21,24 @@ function GetPreview() {
   fetch(`https://api.linkpreview.net/?key=44ab599c49f52aa82bb84040cf5d3748&q=${input.value}`)
     .then(res => res.json())
     .then(json => {
-      const url = new URL(json.url);
+      let url;
+      if (!json.error) url = new URL(json.url);
       const html = ` 
        <div class="img-cont">
         <img src="${json.image}" onerror="addimg(this)">
        </div>
        <div class="details">
         <h3>${json.title}</h3>
-        <h5>${json.description}</h5>
-        <a href="${json.url}">${url.hostname}</a>
+        ${json.description && `<h5>${json.description}</h5>`}
+        <a href="${json.url}">${url && url.hostname}</a>
         </div>`;
-      
-      NewElment.style.border = '2px solid cornflowerblue';
-      NewElment.innerHTML = html;
-      console.log(json.image)
+      if (json.error) {
+        alert(json.description);
+        NewElment.remove();
+      }else {
+        NewElment.style.border = '2px solid cornflowerblue';
+        NewElment.innerHTML = html;
+      }
     });
 }
 
