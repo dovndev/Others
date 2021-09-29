2b61c86205f03e2a4268f0fd1e80765c4629a3f4
noelpaul <rosytomy101@gmail.com>
2021-09-29 22:28:23 +0530

    updated

--- Birthday/main.js
+++ Birthday/main.js
@@ -4,27 +4,23 @@ const cover = document.querySelector('.cover');
 let index = 0;
 
 function ChangeSrc() {
-  if(index > 10)index = 0;
-  const src = `./Images/Img${index}.jpg`;
+  if (index > 10) index = 0;
   img.classList.add('blur');
-  setTimeout(() => {
-    img.src = src;
-    img.classList.remove('blur')
+  setTimeout(() => img.src = `./Images/Img${index}.jpg`, 500)
+  img.onload = () => {
+    setTimeout(ChangeSrc, 4000);
+    setTimeout(() => img.classList.remove('blur'), 500);
     index++;
-  }, 1000)
-}
-
-function loadImages() {
-  let i;
-  for (i = 0; i < 11; i++) {
-    fetch(`./Images/Img${i}.jpg`)
+  }
+  
+  img.onerror = () => {
+    index++;
+    ChangeSrc();
   }
 }
 
-window.onload = loadImages;
-
 function Start() {
-  setInterval(ChangeSrc , 4000);
+  setTimeout(ChangeSrc , 4000);
   cover.style.display = 'none';
   song.controls = false;
   song.playbackRate = 0.6;
