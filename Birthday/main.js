7ba66a9fe83ebaa9ed7b9411f80b7573bbcc2faf
noelpaul <rosytomy101@gmail.com>
2021-09-29 20:29:09 +0530

    updated

---
+++ Birthday/main.js
@@ -0,0 +1,22 @@
+const img = document.getElementById('img');
+const song = document.getElementById('song');
+const cover = document.querySelector('.cover');
+let index = 0;
+
+function ChangeSrc() {
+  if(index > 10)index = 0;
+  const src = `./Images/Img${index}.jpg`;
+  img.classList.add('blur');
+  setTimeout(() => {
+    img.src = src;
+    img.classList.remove('blur')
+    index++;
+  }, 1000)
+}
+function Start() {
+  setInterval(ChangeSrc , 4000);
+  cover.style.display = 'none';
+  song.controls = false;
+  song.playbackRate = 0.6;
+  song.play();
+}
