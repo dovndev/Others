0a2544d6fd85a0c638d8b6a64a897551a1904a16
Home <home@pop-os.localdomain>
2021-11-02 23:44:10 +0530

    notes updated

--- Notes/react-ui.js
+++ Notes/react-ui.js
@@ -1,6 +1,5 @@
 const { useEffect, useState, useCallback, useRef } = React;
 
-
 const useLocalStorage = (key, initialvalue) => {
   const [value, setvalue] = useState(initialvalue);
   useEffect(() => {
@@ -20,11 +19,10 @@ const Mapper = ({ notes, handledelete, HandleEdit }) => {
       return (
         <div className="notes" key={item.id}>
           <button
-            onClick={e => HandleEdit(item.id)}
-            class={`delete edit${item.completed ? ' completed':''}`}
-            style={{fontSize: `${item.completed ? 24 : 30}px`}}
+            onClick={(e) => HandleEdit(item.id)}
+            class={`delete edit${item.completed ? " completed" : ""}`}
           >
-            {item.completed ? '\u2715' : '\u2713'}
+            {item.completed ? "✕" : "✓"}
           </button>
           <p>
             {item.body.map((line) => (
@@ -32,7 +30,7 @@ const Mapper = ({ notes, handledelete, HandleEdit }) => {
             ))}
           </p>
           <button onClick={(e) => handledelete(e, item.id)} className="delete">
-            &times;
+            ✕
           </button>
         </div>
       );
@@ -60,13 +58,29 @@ const App = () => {
     updateInputSize();
   }, [newNote]);
 
+  useEffect(() => {
+    const elmnt = document.querySelector(".root");
+    const themeColor = theme ? "#ffffff" : "#161b22";
+    if (theme) elmnt.classList.remove("dark");
+    else elmnt.classList.add("dark");
+    const meta = document.getElementsByTagName("meta");
+    const list = [
+      "theme-color",
+      "msapplication-TileColor",
+      "apple-mobile-web-app-status-bar"
+    ]
+    for (let i = 0; i < meta.length; i++) {
+      if (list.includes(meta[i].name)) meta[i].content = themeColor;
+    }
+  }, [theme]);
+
   function CheckIsNote() {
     setisNote(!/^\s*$/g.test(newNote));
   }
 
   const updateInputSize = useCallback(() => {
     const el = textarea.current;
-    el.style.height = "25px";
+    el.style.height = "30px";
     el.style.height = el.scrollHeight + "px";
     el.style.overflowY = el.scrollHeight < 130 ? "hidden" : "auto";
   }, [newNote]);
@@ -77,7 +91,7 @@ const App = () => {
         {
           id: new Date().getTime(),
           body: newNote.trim().split(/\n/g),
-          completed: false
+          completed: false,
         },
         ...notes,
       ]);
@@ -96,15 +110,18 @@ const App = () => {
     [notes]
   );
 
-  const HandleEdit = useCallback((id) => {
-    const Newnotes = notes.map(item => {
-      if (item.id === id) {
-        item.completed = !item.completed;
-        return item;
-      }else return item;
-    });
-    setnotes(Newnotes);
-  }, [notes])
+  const HandleEdit = useCallback(
+    (id) => {
+      const Newnotes = notes.map((item) => {
+        if (item.id === id) {
+          item.completed = !item.completed;
+          return item;
+        } else return item;
+      });
+      setnotes(Newnotes);
+    },
+    [notes]
+  );
 
   const removeall = useCallback(() => {
     if (confirm(`Delete All Notes`)) {
@@ -147,36 +164,53 @@ const App = () => {
 
   return (
     <div className={theme ? "html" : "html dark"}>
-      <div className="header">
-        <p>Notebook</p>
-        <span onClick={handleNav}>&#9776;</span>
+      <div className="header-cont">
+        <div className="header">
+          <p>Notebook</p>
+          <span onClick={handleNav}>
+            <svg viewBox="0 0 24 24">
+              <path
+                fill="white"
+                d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"
+              ></path>
+            </svg>
+          </span>
+        </div>
       </div>
       <div className="form" onSubmit={saveNote}>
         <textarea
           className={isNote ? "textarea" : "notextarea"}
           rows={1}
           ref={textarea}
-          placeholder="Type a note"
           value={newNote}
           onChange={(e) => handlechange(e)}
         ></textarea>
+        <span className="placeholder">Type a note</span>
         <button onClick={saveNote} className={isNote ? "save" : "nosave"}>
           <span>save</span>
         </button>
       </div>
 
       <div className="container">
-        {notes && <Mapper notes={notes} handledelete={handledelete} HandleEdit={HandleEdit} />}
+        {notes && (
+          <Mapper
+            notes={notes}
+            handledelete={handledelete}
+            HandleEdit={HandleEdit}
+          />
+        )}
       </div>
 
-      <div className="nav-cont" onClick={handleNav} style={{display: nav ? "block" : "none"}}></div>
+      <div
+        className={nav ? "nav-cont nav-cont-show" : "nav-cont"}
+        onClick={handleNav}
+      ></div>
 
-      <div className="nav" style={{display: nav ? "block" : "none"}}>
+      <div className={nav ? "nav nav-show" : "nav"}>
         <div onClick={handleEnter}>Enter is save</div>
         <div onClick={() => settheme(!theme)}>Theme</div>
-        <div onClick={removeall}>Clear storage</div>
+        <div onClick={removeall}>Delete All Notes</div>
       </div>
-
     </div>
   );
 };
--- React/index.js
+++ React/index.js
@@ -1,37 +1,118 @@
-const { useState } = React;
+const { useState, useEffect, useMemo } = React;
+
+const useFetch = (url) => {
+  const [data, setData] = useState();
+  const [error, setError] = useState();
+  const [loading, setLoading] = useState(true);
+
+  const loadData = () => {
+    setData(undefined);
+    setLoading(true);
+    setError(false);
+    fetch(url)
+      .then((res) => res.json())
+      .then(setData)
+      .catch(setError)
+      .finally(() => setLoading(false));
+  };
+
+  useEffect(loadData, []);
+
+  return [loading, error, data, loadData];
+};
+
+const Posts = ({ posts }) => {
+  return (
+    <div className="post-list">
+      {posts.map((post) => {
+        return (
+          <div className="post" key={post.id}>
+            <h2>{post.title}</h2>
+            <p>{post.body}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
+
+const useToggle = (initialValue) => {
+  const [value, setValue] = useState(initialValue);
+
+  const toggleValue = () => {
+    setValue(!value);
+  };
+
+  return [value, toggleValue];
+};
+
+const useLocalStorage = (key, initialValue) => {
+  const [value, setValue] = useState();
+
+  useEffect(() => {
+    const savedData = localStorage.getItem(key);
+    if (savedData) setValue(JSON.parse(savedData));
+    else setValue(initialValue);
+  }, []);
+
+  useEffect(() => {
+    localStorage.setItem(key, JSON.stringify(value));
+  }, [value]);
+
+  const removevalue = () => {
+    localStorage.removeItem(key);
+  };
+
+  return [value, setValue, removevalue, key];
+};
 
 const App = () => {
-  const Name = "M4tech";
+  const [name, setName, removeName] = useLocalStorage("name", "noel");
+  const [age, setAge, removeAge] = useLocalStorage("age", 15);
+  const [darktheme, toggleTheme] = useToggle(false);
+  const [loading, error, posts, reload] = useFetch(
+    "https://jsonplaceholder.typicode.com/posts"
+  );
+
+  const style = useMemo(() => {
+    return {
+      backgroundColor: darktheme ? "black" : "white",
+      color: darktheme ? "white" : "black",
+    };
+  }, [darktheme]);
+
+  useEffect(() => {
+    console.log("changed");
+  }, [style]);
 
   return (
-    <div className="page">
-      <Header />
+    <div style={style} className="cont">
+      <h1>
+        Hello, ninja {name} at {age}
+      </h1>
+      Name
+      <input
+        type="text"
+        onChange={(e) => setName(e.target.value)}
+        value={name}
+      />
+      <button onClick={removeName}>remove name</button>
+      <br />
+      Age
+      <input
+        type="number"
+        onChange={(e) => setAge(e.target.value)}
+        value={age}
+      />
+      <button onClick={removeAge}>remove Age</button>
+      <br />
+      <button onClick={toggleTheme}>{darktheme ? "light" : "dark"}theme</button>
+      {!loading && <button onClick={reload}>reload data</button>}
+      {loading && <h3>Loading...</h3>}
+      {posts && <Posts posts={posts} />}
+      {error && <div>error occured</div>}
     </div>
   );
 };
 
-const Header = () => {
-  const icon =
-    "https://yt3.ggpht.com/ytc/AKedOLS-Asy0HhW7tk9pJ-q4ZJo1sKPRaKdMkKH6490GxA=s176-c-k-c0x00ffffff-no-rj-mo";
-  const Name = "M4tech";
-
-  return (
-    <div className="header">
-      <div className="left">
-        <img src={icon}></img>
-        <p>{Name}</p>
-      </div>
-
-      <div className="right">
-        <ul>
-          <li>Home</li>
-          <li>Home</li>
-          <li>Home</li>
-          <li>Home</li>
-          <li>Home</li>
-        </ul>
-      </div>
-    </div>
-  );
-};
 ReactDOM.render(<App />, document.getElementById("app"));
