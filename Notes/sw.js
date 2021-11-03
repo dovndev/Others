adae0e05c60f1ee914c8670e453b2da585d4fc50
Home <home@pop-os.localdomain>
2021-11-03 10:37:46 +0530

    version 0.2 Notes at 10:37 AM

--- Notes/sw.js
+++ Notes/sw.js
@@ -1,4 +1,4 @@
-const version = 0.1;
+const version = 0.2;
 const staticCacheName = `site-shell-assets-v${version}`;
 const dynamicCacheName = `site-dynamic-assets-v${version}`;
 const dynamicCacheLimit = 15;
