c2a43d08048d66a55d42cf3a43b25f0cb5f380fe
Home <home@pop-os.localdomain>
2021-11-03 22:38:31 +0530

    version 1 NoteBook at 10:25 PM

--- Notes/app.js
+++ Notes/app.js
@@ -1,35 +1,31 @@
-const APP = {
-  theme: JSON.parse(localStorage.getItem("Theme-React")),
+const theme = JSON.parse(localStorage.getItem("Theme-React")),
+  firstInstall = JSON.parse(localStorage.getItem("FirstInstall")),
+  reloadConfirm =
+    "New update have been installed. let's Activate the update by reloading the page";
 
-  firstInstall: JSON.parse(localStorage.getItem("FirstInstall")),
+const APPinit = () => {
+  if ("serviceWorker" in navigator) {
+    navigator.serviceWorker.addEventListener("controllerchange", () => {
+      if (firstInstall === null) {
+        localStorage.setItem("FirstInstall", false);
+      }
 
-  reloadConfirm:
-    "New update have been installed. let's Activate the update by reloading the page",
+      if (firstInstall === false) {
+        if (confirm(reloadConfirm)) window.location.reload();
+      }
+    });
 
-  init: () => {
-    if ("serviceWorker" in navigator) {
-      navigator.serviceWorker.addEventListener("controllerchange", () => {
-        if (firstInstall === null) {
-          localStorage.setItem("FirstInstall", false);
-        }
+    navigator.serviceWorker
+      .register("./sw.js")
+      .then(() => console.log("Service-Worker Registered"))
+      .catch((err) => console.log("Service-Worker Not Registered", err));
+  }
 
-        if (firstInstall === false) {
-          if (confirm(reloadConfirm)) window.location.reload();
-        }
-      });
-
-      navigator.serviceWorker
-        .register("./sw.js")
-        .then(() => console.log("Service-Worker Registered"))
-        .catch((err) => console.log("Service-Worker Not Registered", err));
-    }
-
-    if (theme !== null) {
-      const elmnt = document.querySelector(".root");
-      if (!theme) elmnt.classList.add("dark");
-      else elmnt.classList.remove("dark");
-    }
-  },
+  if (theme !== null) {
+    const elmnt = document.querySelector(".root");
+    if (!theme) elmnt.classList.add("dark");
+    else elmnt.classList.remove("dark");
+  }
 };
 
-document.addEventListener("DOMContentLoaded", APP.init);
+document.addEventListener("DOMContentLoaded", APPinit);
