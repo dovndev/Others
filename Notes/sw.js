7fbf326263ffdea8454ab4a987f07323b6ad515f
VAIO <vaio@pop-os.localdomain>
2021-12-26 11:29:15 +0530

    maskable icon

--- Notes/sw.js
+++ Notes/sw.js
@@ -1,4 +1,4 @@
-const version = 0.35;
+const version = 0.36;
 const staticCacheName = `site-shell-assets-v${version}`;
 const dynamicCacheName = `site-dynamic-assets-v${version}`;
 const dynamicCacheLimit = 15;
