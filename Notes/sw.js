84e843f2af6f8f3c2db110d8b3b55b6530f50548
VAIO <vaio@pop-os.localdomain>
2022-03-29 18:00:09 +0530

    notebook error fix

--- Notes/sw.js
+++ Notes/sw.js
@@ -1,4 +1,4 @@
-const version = 0.234;
+const version = 0.237774;
 const staticCacheName = `site-shell-assets-v${version}`;
 const dynamicCacheName = `site-dynamic-assets-v${version}`;
 const dynamicCacheLimit = 15;
