367dc893b2c53b65f4e97c543edd4f0dd2f44d55
VAIO <vaio@pop-os.localdomain>
2022-03-29 18:27:51 +0530

    notebook error fix

--- Notes/react-ui.js
+++ Notes/react-ui.js
@@ -162,14 +162,14 @@ const App = () => {
       <div className="header-cont">
         <div className="header">
           <p>Notebook</p>
-          <span onClick={() => setnav(!nav)}>
+          <button onClick={() => setnav(!nav)}>
             <svg viewBox="0 0 24 24">
               <path
                 fill="white"
                 d="M3 18h18v-2H3v2zm0-5h18v-2H3v2zm0-7v2h18V6H3z"
               ></path>
             </svg>
-          </span>
+          </button>
         </div>
       </div>
       <div className="form">
