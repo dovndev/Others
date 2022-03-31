5c1d4411e1863b11676f38e6dae5b88b513263f7
VAIO <vaio@pop-os.localdomain>
2022-03-31 10:58:56 +0530

    notebook icon fix

--- Notes/sw.js
+++ Notes/sw.js
@@ -1,4 +1,4 @@
-const version = 11;
+const version = 4;
 const staticCacheName = `site-shell-assets-v${version}`;
 const dynamicCacheName = `site-dynamic-assets-v${version}`;
 const dynamicCacheLimit = 15;
