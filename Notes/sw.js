d81b524238355dad7ae2fc0a2d2aa12f21f57e5a
VAIO <vaio@pop-os.localdomain>
2022-04-05 15:26:46 +0530

    notebook cursor fix

--- Notes/sw.js
+++ Notes/sw.js
@@ -1,4 +1,4 @@
-const version = 5;
+const version = 64;
 const staticCacheName = `site-shell-assets-v${version}`;
 const dynamicCacheName = `site-dynamic-assets-v${version}`;
 const dynamicCacheLimit = 15;
