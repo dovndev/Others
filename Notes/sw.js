223cf98d7b2822755b02990e9765fb0a3a69be79
VAIO <vaio@pop-os.localdomain>
2022-04-06 22:20:47 +0530

    notebook colour fix

--- Notes/sw.js
+++ Notes/sw.js
@@ -1,4 +1,4 @@
-const version = 64422222;
+const version = 111;
 const staticCacheName = `site-shell-assets-v${version}`;
 const dynamicCacheName = `site-dynamic-assets-v${version}`;
 const dynamicCacheLimit = 15;
