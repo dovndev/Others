13fcab41bd35ddf11582004225a131b7ac179479
noelpaul <rosytomy101@gmail.com>
2022-05-11 19:18:58 +0530

    note-book- update

--- Notes/sw.js
+++ Notes/sw.js
@@ -1,4 +1,4 @@
-const version = 13;
+const version = 132;
 const staticCacheName = `site-shell-assets-v-${version}`;
 const dynamicCacheName = `site-dynamic-assets-v-${version}`;
 const dynamicCacheLimit = 15;
