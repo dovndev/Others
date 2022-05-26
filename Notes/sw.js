ecea252e743c82e27ad0172f2f2f3e20040ecf97
noelpaul <rosytomy101@gmail.com>
2022-05-26 22:00:51 +0530

    note-book-buttons

--- Notes/sw.js
+++ Notes/sw.js
@@ -1,4 +1,4 @@
-const version = 1.030;
+const version = 1.031;
 const staticCacheKey = `shell-assets-version-${version}`;
 const dynamicCacheKey = `extra-assets-version-${version}`;
 const dynamicCacheLimit = 15;
