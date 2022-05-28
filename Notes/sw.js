1e11977eb5da8588a42b36198a348a36e2663673
noelpaul <rosytomy101@gmail.com>
2022-05-28 11:34:48 +0530

    note-book-nav-buttons

--- Notes/sw.js
+++ Notes/sw.js
@@ -1,4 +1,4 @@
-const version = 1.036;
+const version = 1.037;
 const staticCacheKey = `shell-assets-version-${version}`;
 const dynamicCacheKey = `extra-assets-version-${version}`;
 const dynamicCacheLimit = 15;
