734f44b071ad6865775ffe9658f5e8d4e1513f98
noelpaul <rosytomy101@gmail.com>
2022-09-03 08:51:33 +0530

    notebook moved

--- Notes/sw.js
+++ Notes/sw.js
@@ -1,4 +1,4 @@
-const version = 1.047;
+const version = 1.049;
 const staticCacheKey = `shell-assets-version-${version}`;
 const dynamicCacheKey = `extra-assets-version-${version}`;
 const dynamicCacheLimit = 15;
